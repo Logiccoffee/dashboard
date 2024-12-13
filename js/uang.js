e884830efe787767d3b289d683554c0659abd17b
balqisrosa <balqisrosasekamayang@gmail.com>
2024-12-13 16:44:15 +0700

    hapus button cetak

--- js/uang.js
+++ js/uang.js
@@ -16,20 +16,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const dropdownItems = document.querySelectorAll(".dropdown-menu .dropdown-item");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const dropdownItems = document.querySelectorAll(".dropdown-menu .dropdown-item");
 
-    dropdownItems.forEach((item) => {
-        item.addEventListener("click", function (event) {
-            event.preventDefault(); // Hindari tindakan default tombol
-            console.log(`Clicked: ${this.id}`); // Debugging
-            if (this.id === "cetakQris") {
-                alert("Fungsi Cetak QRIS diaktifkan!");
-                // Tambahkan logika cetak QRIS di sini
-            } else if (this.id === "cetakCash") {
-                alert("Fungsi Cetak Cash diaktifkan!");
-                // Tambahkan logika cetak Cash di sini
-            }
-        });
-    });
-});
+//     dropdownItems.forEach((item) => {
+//         item.addEventListener("click", function (event) {
+//             event.preventDefault(); // Hindari tindakan default tombol
+//             console.log(`Clicked: ${this.id}`); // Debugging
+//             if (this.id === "cetakQris") {
+//                 alert("Fungsi Cetak QRIS diaktifkan!");
+//                 // Tambahkan logika cetak QRIS di sini
+//             } else if (this.id === "cetakCash") {
+//                 alert("Fungsi Cetak Cash diaktifkan!");
+//                 // Tambahkan logika cetak Cash di sini
+//             }
+//         });
+//     });
+// });
