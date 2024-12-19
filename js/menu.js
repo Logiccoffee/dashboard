45053a90507a999c83711b5dd20d50d9cc4e0c49
AudyardhaNasywa <audyardhaandini@gmail.com>
2024-12-19 17:40:10 +0700

    menampilkan preview gambar

--- js/menu.js
+++ js/menu.js
@@ -389,7 +389,15 @@ function openEditMenuPopup(menuId) {
         document.getElementById('edit-product-price').value = parsePrice(menu.price);
         document.getElementById('edit-product-description').value = menu.description;
         document.getElementById('edit-product-status').value = menu.status;
-        document.getElementById('edit-product-image').setAttribute('data-old-image', menu.image); // Menyimpan gambar lama
+        // Mengupdate gambar lama dan menampilkannya di preview
+        const imageUrl = menu.image; // Gambar yang diterima dari server
+        const imagePreviewElement = document.getElementById('edit-product-image-preview');
+        if (imageUrl) {
+            imagePreviewElement.src = imageUrl; // Menampilkan gambar di preview
+        }
+
+        // Menyimpan gambar lama sebagai data-old-image (untuk referensi jika tidak diganti)
+        document.getElementById('edit-product-image').setAttribute('data-old-image', imageUrl);
 
         // Pastikan event listener ini hanya berjalan sekali
         modalElement.removeEventListener('shown.bs.modal', onModalShown);
