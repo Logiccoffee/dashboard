7608b27585f43226bd3a33302a12098c616cf930
AudyardhaNasywa <audyardhaandini@gmail.com>
2024-12-19 17:07:49 +0700

    update lagiwww

--- js/menu.js
+++ js/menu.js
@@ -389,11 +389,7 @@ function openEditMenuPopup(menuId) {
         document.getElementById('edit-product-price').value = parsePrice(menu.price);
         document.getElementById('edit-product-description').value = menu.description;
         document.getElementById('edit-product-status').value = menu.status;
-        document.getElementById('edit-product-old-image').value = menu.image || '';
-
-        // Debugging gambar lama
-        console.log("Gambar lama:", menu.image);
-        document.getElementById('old-image-url').textContent = `Gambar Lama: ${menu.image}`;
+        document.getElementById('edit-product-image').setAttribute('data-old-image', menu.image); // Menyimpan gambar lama
 
         // Pastikan event listener ini hanya berjalan sekali
         modalElement.removeEventListener('shown.bs.modal', onModalShown);
